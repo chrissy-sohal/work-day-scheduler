4d670a6f1f296e59802ccf803f615b9255f2f567
C Martin <cmart131@yahoo.com>
2022-12-10 14:55:10 -0800

    added code to js file, html file, and some of readme

--- script.js
+++ script.js
@@ -1,54 +1,47 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 $(document).ready(function () {
-
- // current day is displayed at the top of the calendar
-$("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
   // saveBtn click listener and save text in local storage
   $(".saveBtn").on("click", function () {
+      
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
 
       localStorage.setItem(time, text);
   })
  
+  //code to apply the past, present, or future class to each time block
   function timeBlock() {
-   var hour = moment().hours();
+    
+    var hour = moment().hours();
 
-   $(".time-block").each(function() {
-       var currHour = parseInt($(this).attr("id"));
-
-       if (currHour > hour) {
-           $(this).addClass("future");
-       } else if (currHour === hour) {
-           $(this).addClass("present");
-       } else {
-           $(this).addClass("past");
-       }
-   })
-  }
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+ 
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+ }
 
   // Get item from local storage
-  $("#hour-8 .description").val(localStorage.getItem("hour8"));
-  $("#hour-9 .description").val(localStorage.getItem("hour9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour17"));
+  $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-  timeBlock();
 })
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+ // Display today's day and date
+ var time = dayjs().format('MMM D, YYYY');
+$('#currentDay').text(time);
